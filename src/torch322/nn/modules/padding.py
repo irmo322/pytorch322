6844555fb4be1c88a5827db080fb4d2d72216b15
Benjamin Maréchal <irmo322@gmail.com>
2026-03-17 02:51:49 +0100

    Ajout du module Padder322.

---
+++ src/torch322/nn/modules/padding.py
@@ -0,0 +1,16 @@
+from torch import nn
+
+from ..functional import pad322
+
+
+class Padder322(nn.Module):
+    
+    def __init__(self, channel_dim, spatial_dims, spatial_paddings, padding_values):
+        super().__init__()
+        self.channel_dim = channel_dim
+        self.spatial_dims = spatial_dims
+        self.spatial_paddings = spatial_paddings
+        self.padding_values = padding_values
+    
+    def forward(self, x):
+        return pad322(x, self.channel_dim, self.spatial_dims, self.spatial_paddings, self.padding_values)
