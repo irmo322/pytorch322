5f7af37a590f78d57f7d7c343816ae87cf06c4f3
Benjamin Maréchal <irmo322@gmail.com>
2026-04-08 12:37:53 +0200

    Rethinking channel normalization.

--- src/torch322/nn/modules/normalization.py
+++ src/torch322/nn/modules/normalization.py
@@ -9,40 +9,51 @@ class ChannelNorm(torch.nn.Module):
     """Applies channel normalization on input tensor.
 
     Values are normalized along the channel dimension.
-    Optionally, a constant channel can be added.
+    Optionally, channels can be prepended to integrate normalization information.
 
     Args:
         channel_dim: The dimension along which channels are normalized.
-        constant_channel: If True, a constant channel will be added.
+        constant_channel: an extra channel with constant value is added before normalization.
         constant_channel_value: The value of the added channel.
-        constant_channel_location: The location of the added channel, "prepend" or "append".
+        factor_log_channel: an extra channel is added after normalization. Its value is the natural log of the
+            normalization factor (one for each vector of channels).
     """
     def __init__(
             self,
             channel_dim,
             constant_channel=False,
             constant_channel_value=1.0,
-            constant_channel_location="prepend",
+            factor_log_channel=False,
     ):
         super().__init__()
+
         self.channel_dim = channel_dim
         self.constant_channel = constant_channel
         self.constant_channel_value = constant_channel_value
-        self.constant_channel_location = constant_channel_location
+        self.factor_log_channel = factor_log_channel
 
     def forward(self, x):
         if self.constant_channel:
-            x_augmented = add_constant_channel(
+            x_with_constant = add_constant_channel(
                 x,
                 self.channel_dim,
-                constant_value=self.constant_channel_value,
-                location=self.constant_channel_location
+                constant_value=self.constant_channel_value
             )
         else:
-            x_augmented = x
+            x_with_constant = x
 
-        n_channels = x_augmented.size()[self.channel_dim]
-        normalisation_tensor = math.sqrt(n_channels) * torch.rsqrt((x_augmented * x_augmented).sum(dim=self.channel_dim, keepdim=True))
-        x_normalized = x_augmented * normalisation_tensor
+        normalization_tensor = _compute_normalization_tensor(x_with_constant, self.channel_dim)
+        x_normalized = x_with_constant * normalization_tensor
 
-        return x_normalized
+        if self.factor_log_channel:
+            x_normalized_with_log = torch.cat((normalization_tensor.log(), x_normalized), dim=self.channel_dim)
+        else:
+            x_normalized_with_log = x_normalized
+
+        return x_normalized_with_log
+
+
+def _compute_normalization_tensor(x, channel_dim):
+    n_channels = x.size()[channel_dim]
+    normalization_tensor = math.sqrt(n_channels) * torch.rsqrt((x * x).sum(dim=channel_dim, keepdim=True))
+    return normalization_tensor
--- tests/test_torch322/test_nn/test_modules/test_normalization.py
+++ tests/test_torch322/test_nn/test_modules/test_normalization.py
@@ -9,7 +9,7 @@ from ...unittest_utils import myAssertAlmostEqual
 
 class TestNormalization(unittest.TestCase):
 
-    def test_channel_norm(self):
+    def test_channel_norm_constant(self):
         channel_norm = ChannelNorm(-2, constant_channel=True)
 
         x = torch.tensor([
@@ -33,6 +33,45 @@ class TestNormalization(unittest.TestCase):
                 [ 1.0000e+00, -1.0000e+00,  0.0000e+00,  0.0000e+00],
             ],
             [
+                [ 2.0000e+00,  1.1547e-03,  5.1640e-01,  6.3246e-01],
+                [ 0.0000e+00,  1.1547e+00,  5.1640e-01, -6.3246e-01],
+                [ 0.0000e+00,  1.1547e+00,  1.0328e+00,  1.2649e+00],
+                [ 0.0000e+00,  1.1547e+00,  1.5492e+00, -1.2649e+00],
+            ]
+        ])
+
+        y = channel_norm(x)
+
+        # print(y)
+
+        myAssertAlmostEqual(self, y, y_expected, relative_delta=0.0001)
+
+    def test_channel_norm_log(self):
+        channel_norm = ChannelNorm(-2, constant_channel=True, factor_log_channel=True)
+
+        x = torch.tensor([
+            [
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, -1.0, -1.0, 0.0],
+                [1.0, -1.0, 0.0, 0.0],
+            ],
+            [
+                [0.0, 1000.0, 1.0, -1.0],
+                [0.0, 1000.0, 2.0, 2.0],
+                [0.0, 1000.0, 3.0, -2.0],
+            ]
+        ])
+
+        y_expected = torch.tensor([
+            [
+                [ 0.0000e+00,  0.0000e+00,  1.4384e-01,  3.4657e-01],
+                [ 1.0000e+00,  1.0000e+00,  1.1547e+00,  1.4142e+00],
+                [ 1.0000e+00,  1.0000e+00,  1.1547e+00,  1.4142e+00],
+                [ 1.0000e+00, -1.0000e+00, -1.1547e+00,  0.0000e+00],
+                [ 1.0000e+00, -1.0000e+00,  0.0000e+00,  0.0000e+00],
+            ],
+            [
+                [ 6.9315e-01, -6.7639e+00, -6.6088e-01, -4.5815e-01],
                 [ 2.0000e+00,  1.1547e-03,  5.1640e-01,  6.3246e-01],
                 [ 0.0000e+00,  1.1547e+00,  5.1640e-01, -6.3246e-01],
                 [ 0.0000e+00,  1.1547e+00,  1.0328e+00,  1.2649e+00],
